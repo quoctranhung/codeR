11a2a89ece26e743b19be8556c3d67026d68d8f2
Tran Hung Quoc <quoc911ndu1819@gmail.com>
2024-04-03 10:28:52 +0700

    Update btl.r

--- btl.r
+++ btl.r
@@ -1,5 +1,18 @@
 library(tidyr)
-
+library(stringr)
 df <- read.csv("All_GPUs.csv")
 View(df)
 
+# Chọn biến
+
+df <- df[ , c("Name", "Max_Power", "Memory_Bandwidth", "Memory_Speed", "Pixel_Rate", "Release_Date", "Texture_Rate")]
+View(df)
+
+# Xử lý dữ liệu
+
+str(df)
+row <- c(1:3406)
+
+df$Max_Power_Nums <- str_extract(df$Max_Power ,"\\d+\\.?\\d*")
+df$Max_Power_Units <- str_replace(df$Max_Power, "\\d+\\.?\\d*", "")
+View(df)
