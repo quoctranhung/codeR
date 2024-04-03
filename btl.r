ac44f3bb27b0842c417d99d36f7f28b9e2be8004
Tran Hung Quoc <quoc911ndu1819@gmail.com>
2024-04-03 23:50:11 +0700

    Update btl.r

--- btl.r
+++ btl.r
@@ -11,8 +11,29 @@ View(df)
 # Xử lý dữ liệu
 
 str(df)
-row <- c(1:3406)
+
+#Tách dữ liệu làm 2 phần giá trị và đơn vị
 
 df$Max_Power_Nums <- str_extract(df$Max_Power ,"\\d+\\.?\\d*")
 df$Max_Power_Units <- str_replace(df$Max_Power, "\\d+\\.?\\d*", "")
+
+df$Memory_Bandwidth_Nums <- str_extract(df$Memory_Bandwidth, "\\d+\\.?\\d*")
+df$Memory_Bandwidth_Units <- str_replace(df$Memory_Bandwidth, "\\d+\\.?\\d*", "")
+
+df$Memory_Speed_Nums <- str_extract(df$Memory_Speed, "\\d+\\.?\\d*")
+df$Memory_Speed_Units <- str_replace(df$Memory_Speed, "\\d+\\.?\\d*", "")
+
+df$Pixel_Rate_Nums <- str_extract(df$Pixel_Rate, "\\d+\\.?\\d*")
+df$Pixel_Rate_Units <- str_replace(df$Pixel_Rate, "\\d+\\.?\\d*", "")
+
+df$Texture_Rate_Nums <- str_extract(df$Texture_Rate, "\\d+\\.?\\d*")
+df$Texture_Rate_Units <- str_replace(df$Texture_Rate, "\\d+\\.?\\d*", "")
+
+# Đặt ngày đầu tiên là mốc 0 và số ngày bắt đầu từ ngày 0 là cột Nums_days
+
+df$Release_Date <- as.Date(sub ("^\\s*\\n", "", df$ Release_Date ), format = "%d-%b-%Y")
+print(min(df$Release_Date, na.rm = TRUE))
+
+
+
 View(df)
